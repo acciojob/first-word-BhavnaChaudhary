3bd007489a0081fbff8413fe48f1bd2b59bb6e5a
BhavnaChaudhary <33368486+BhavnaChaudhary@users.noreply.github.com>
2025-03-17 15:30:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function firstWord(s) {
-  // your code here
+
+function firstWord(str) {
+    if (!str) return str; // Return empty string if input is empty or null
+    let index = str.indexOf(' ');
+    return index === -1 ? str : str.substring(0, index);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+const str = prompt("Enter String:");
+//alert(firstWord(s));
